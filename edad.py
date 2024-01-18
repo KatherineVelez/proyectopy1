051fffb1abdae2ec54122f8394b7befd6cba9d47
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-18 07:39:02 -0500

    se agrego nuevos archivos

---
+++ edad.py
@@ -0,0 +1,2 @@
+nombre = input("ingrese por favor su nombre: ")
+Edad = input("ingrese por favor su nombre: ")
---
+++ par.py
@@ -0,0 +1,8 @@
+numero = int(input("ingrese un numero entero positivo: "))
+if numero %2 == 0:
+    print(f"El numero {numero} es par")
+else:
+    print(f"El numero {numero} es impar")
+
+
+       
